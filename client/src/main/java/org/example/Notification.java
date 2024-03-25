c5ae7bb43c4112b5c3ecab82393a6f3ed7897c8e
245965 <245965@edu.p.lodz.pl>
2024-03-25 18:33:18 +0100

    Creating Notification class

---
+++ client/src/main/java/org/example/Notification.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class Notification {
+     private String text;
+     private int time;
+
+    public Notification(String text, int time) {
+        this.text = text;
+        this.time = time;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+}
