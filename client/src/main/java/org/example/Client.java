8b2edf1891b06a410be8f00965526ba7e18e48c8
245965 <245965@edu.p.lodz.pl>
2024-03-25 18:49:27 +0100

    Creating Client class with methods connect(),disconnect()

--- client/src/main/java/org/example/Client.java
+++ client/src/main/java/org/example/Client.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.io.*;
+import java.net.Socket;
+
 public class Client {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private Socket socket;                      //socket declaration.
+    private BufferedReader bufferedReader;      //bufferReader declaration.
+    private BufferedWriter bufferedWriter;
+
+    public void connect(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    public void disconnect() throws IOException{
+        socket.close();
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
