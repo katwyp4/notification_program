7391e9db33c7ebd69dd07f35d0365085f01de2e2
245965 <245965@edu.p.lodz.pl>
2024-04-02 18:50:34 +0200

    Starting Client main method with connecting to server

--- client/src/main/java/org/example/Client.java
+++ client/src/main/java/org/example/Client.java
@@ -8,15 +8,54 @@ public class Client {
     private BufferedReader bufferedReader;      //bufferReader declaration.
     private BufferedWriter bufferedWriter;
 
+    public static void main(String[] args) {
+        try{
+            Client client = new Client();
+            client.connect("localhost", 6969);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     public void connect(String host, int port) throws IOException {
         socket = new Socket(host, port);
         bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        System.out.println("Server connected: " + host + ":" + port);
     }
 
     public void disconnect() throws IOException{
         socket.close();
         bufferedReader.close();
         bufferedWriter.close();
+    }
+    public void reciveMessage() throws IOException{
+        new Thread(() ->{
+            while (!socket.isClosed()){
+                try {
+                    String message = bufferedReader.readLine();
+                    if (message != null){
+                        System.out.println("Message: " +message);
+                    }else {
+                        break;
+                    }
+                } catch (IOException e) {
+                    System.out.println("Error receiving message");
+                    try {
+                        disconnect();
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    break;
+                }
+            }
+        }).start();
+    }
+    private void send(Notification notification) throws IOException{
+        String notificationMessage = notification.getText() + "|" + notification.getTime();
+        bufferedWriter.write(notificationMessage);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
     }
 }
